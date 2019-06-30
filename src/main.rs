0fd3444f2b692a0e39c5f8d0e44b8221be13a79f
Joel Palmer <joelpalmer@gmail.com>
2019-06-30 11:25:03 -0500

    print out first Const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Inspecting the processes's environment...");
+    println!("Here are the constants associated with the current target:");
+    print_constants();
+}
+
+fn print_constants() {
+    let arch = env::consts::ARCH;
+    println!("Architecture of the current CPU is {}", arch);
 }
