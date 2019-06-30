b81aa5b665c6b27096fc5ea49abe6f54f4bd531a
Joel Palmer <joelpalmer@gmail.com>
2019-06-30 11:52:21 -0500

    finish Consts

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,41 @@ fn main() {
 
 fn print_constants() {
     let arch = env::consts::ARCH;
-    println!("Architecture of the current CPU is {}", arch);
+    println!("Architecture of the current CPU is: {}", arch);
+
+    let dll_extension = env::consts::DLL_EXTENSION;
+    println!(
+        "file extension used for shared libraries on this platform: {}",
+        dll_extension
+    );
+
+    let dll_prefix = env::consts::DLL_PREFIX;
+    println!(
+        "Filename prefix used for shared libraries on this platform: {}",
+        dll_prefix
+    );
+
+    let dll_suffix = env::consts::DLL_SUFFIX;
+    println!(
+        "Filename suffix used for shared libraries on this platform: {}",
+        dll_suffix
+    );
+
+    let exe_extension = env::consts::EXE_EXTENSION;
+    println!(
+        "File extension, if any, used for executable binaries on this platform: {}",
+        exe_extension
+    );
+
+    let exe_suffix = env::consts::EXE_SUFFIX;
+    println!(
+        "File extension, if any, used for executable binaries on this platform: {}",
+        exe_suffix
+    );
+
+    let os_family = env::consts::FAMILY;
+    println!("Operating system family: {}", os_family);
+
+    let operating_system = env::consts::OS;
+    println!("Specific Operating system in use: {}", operating_system);
 }
